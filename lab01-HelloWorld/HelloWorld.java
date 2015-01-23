f471922f8012bcbc36b558a73176cd8a6ed898e2
der217 <der217@lehigh.edu>
2015-01-23 16:59:13 +0000

    updated lab01

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////
+//Deepankar Roy
+//Demo 0
+//Hello World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      Java HelloWorld//
+
+public class HelloWorld{
+    
+    //add main method
+    public static void main(String[] args){
+    	System.out.println("###############################");
+    	System.out.println("###############################");
+    	System.out.println("#       Hello World 2015      #");
+    	System.out.println("###############################");
+    	System.out.println("###############################");
+    	System.out.println("test");
+    }
+
+    
+}
